1d6443951c45589055f83c3fbb359d84aa895435
Chad Hansen <chansen.306@gmail.com>
2015-09-26 13:57:13 -0600

    Added stubbed out weapon script. CH

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+
+	public void Fire(Player player){
+	
+	}
+	
+	public void Morph(Weapon w){
+	
+	}
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
