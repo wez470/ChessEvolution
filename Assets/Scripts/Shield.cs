36bda20c2b8618eb5bb61b00b2bf8a26e55ba816
Jesse Rolheiser <jlr297@gmail.com>
2015-09-26 21:58:06 -0600

    Fixed hacky shield, JLR

--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -3,15 +3,13 @@ using System.Collections;
 
 public class Shield : MonoBehaviour {
 
-	private bool isEnabled;
 	private float lastActivationTime;
 
 	private const float DEFAULT_ENABLED_TIME = 3.0f;
-	private const float DEFAULT_ACTIVATION_DELAY = 5.0f;
+	private const float DEFAULT_ACTIVATION_DELAY = 6.0f;
 	
     // Use this for initialization
     void Start () {
-		isEnabled = false;
 		lastActivationTime = Time.time - DEFAULT_ACTIVATION_DELAY;
     }
     
@@ -23,7 +21,7 @@ public class Shield : MonoBehaviour {
 	private bool canEnable(){
 		if (lastActivationTime + DEFAULT_ENABLED_TIME >= Time.time) {
 			return true;
-		} else if (lastActivationTime + DEFAULT_ACTIVATION_DELAY >= Time.time) {
+		} else if (lastActivationTime + DEFAULT_ACTIVATION_DELAY > Time.time) {
 			return false;
 		} else {
 			return true;
@@ -31,16 +29,15 @@ public class Shield : MonoBehaviour {
 	}
 
 	public void enabled(bool isShieldButtonDown){
-		if (!isEnabled && isShieldButtonDown && canEnable ()) {
+		if(isShieldButtonDown && lastActivationTime + DEFAULT_ACTIVATION_DELAY < Time.time ){
 			lastActivationTime = Time.time;
-			isEnabled = isShieldButtonDown;
-		} else if (isEnabled && isShieldButtonDown && canEnable ()) {
-			isEnabled = isShieldButtonDown;
-		} else {
-			isEnabled = false;
 		}
 
-		gameObject.SetActive (isEnabled);
+		if (isShieldButtonDown && canEnable()) {
+			gameObject.SetActive (true);
+		} else {
+			gameObject.SetActive (false);
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
