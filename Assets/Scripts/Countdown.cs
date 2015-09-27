867b60fc3321f7e623856d5321298bb68a6cfe07
Weston <wez470@gmail.com>
2015-09-26 22:38:58 -0600

    Add Countdown. WRC

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Countdown : MonoBehaviour {
+    public int countdownStart = 122;
+	
+    private int timeLeft;
+
+    void Start() {
+        timeLeft = countdownStart;
+    }
+
+    void Update () {
+        timeLeft = (int)(countdownStart - Time.realtimeSinceStartup);
+        int minutesLeft = timeLeft / 60;
+        int secondsLeft = timeLeft % 60;
+        string timeToDisplay = minutesLeft.ToString("00") + ":" + secondsLeft.ToString("00");
+        GetComponent<Text>().text = timeToDisplay;
+	}
+
+    public float GetPercentMatchDone() {
+        return 100.0f - ((float)timeLeft / countdownStart * 100.0f);
+    }
+}
